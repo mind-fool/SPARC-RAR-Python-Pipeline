7cf10191ecd1d8f9caaa1566cb56a6801fa03b5b
Nirajan rimal <nirajanrimal@Nirajans-MacBook-Pro.local>
2025-11-10 15:52:08 -0500

    Edited to call from main

--- filter_high_error_data.py
+++ filter_high_error_data.py
@@ -1,12 +1,13 @@
 from data_preprocess import load_sparc_dataset
-def apply_quality_cut(threshold=0.2):
+
+def filter_data(threshold=0.2):
     df = load_sparc_dataset()
     df['rel_err'] = df['eV_kms'] / df['Vobs_kms']
 
     filtered = df[df['rel_err'] < threshold]
-
     filtered.to_csv("filtered_data.csv", index=False)
     print(f"Saved {filtered.shape[0]} rows to filtered_data.csv")
+    return filtered
 
 if __name__ == "__main__":
-    apply_quality_cut()
+    filter_data()
